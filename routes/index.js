a2834fdf3c72fa3d8fbeaaf65d7516a4da94d938
Pjotr Savitski <pjotr.savitski@gmail.com>
2014-09-22 14:07:16 +0300

    Renamed app and ignores the css file.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'i18n-test' });
 });
 
 module.exports = router;
