2ec520fa8d2de01c588be4eb1cc0d62b2298f5b6
Pjotr Savitski <pjotr.savitski@gmail.com>
2014-09-30 18:38:28 +0300

    Made changes to static files.
    CSS generation middleware was no longer functional. Changed the file
    structure to comply.

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public/static')));
 app.use(i18n.init);
 
 // Middleware to add available locales to locals
